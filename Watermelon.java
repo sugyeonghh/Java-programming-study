c8690a85cc53b7a8aef6e163e5f96501ecfbdcdf
sugyeonghh <sugyeong.py@gmail.com>
2022-11-08 23:19:47 +0900

    수박수박수박수박

---
+++ Watermelon.java
@@ -0,0 +1,16 @@
+public class Watermelon {
+    public String solution(int n) {
+        String word = "수박";
+        if (n <= 2) return word.substring(0, n);
+
+        String answer = word.repeat(n/2);
+        if (n % 2 == 1) answer += word.substring(0, 1);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Watermelon watermelon = new Watermelon();
+        System.out.println("n이 3인 경우: " + watermelon.solution(3));
+        System.out.println("n이 4인 경우: " + watermelon.solution(4));
+    }
+}
