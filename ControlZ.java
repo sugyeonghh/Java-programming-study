1dda2b13e3977e63c390140167fc8e8dc36742b4
sugyeonghh <sugyeong.py@gmail.com>
2022-11-12 23:32:29 +0900

    컨트롤 제트

---
+++ ControlZ.java
@@ -0,0 +1,27 @@
+// 컨트롤 제트
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ControlZ {
+    public int solution(String s) {
+        ArrayList<String> items = new ArrayList<>(Arrays.asList(s.split(" ")));
+        String z = "Z";
+        int sum = 0;
+
+        while (items.contains(z)) {
+            items.remove(items.indexOf(z) - 1);
+            items.remove(items.indexOf(z));
+        }
+        for (int i = items.size() - 1; i >= 0; i--)
+            sum += Integer.parseInt(items.get(i));
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ControlZ controlZ = new ControlZ();
+        System.out.println(controlZ.solution("1 2 Z 3"));
+        System.out.println(controlZ.solution("10 20 30 40"));
+        System.out.println(controlZ.solution("10 Z 20 Z 1"));
+    }
+}
