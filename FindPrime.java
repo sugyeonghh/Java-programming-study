6f4e579761b9bdde4bb26fd4d6669c77fdef1eb2
sugyeonghh <sugyeong.py@gmail.com>
2022-11-17 23:04:07 +0900

    소수 찾기

---
+++ FindPrime.java
@@ -0,0 +1,25 @@
+// 프로그래머스 - 소수 찾기
+
+public class FindPrime {
+    public int solution(int n) {
+        int count = 0;
+        for (int i = 2; i <= n; i++)
+            if (isPrime(i))
+                count++;
+        return count;
+    }
+
+    private boolean isPrime(int n) {
+        if (n == 2) return true;
+        else if (n == 1 || n % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(n); i += 2)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        FindPrime findPrime = new FindPrime();
+        System.out.println(findPrime.solution(10));
+        System.out.println(findPrime.solution(5));
+    }
+}
