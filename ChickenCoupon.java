5096c36d800f827cb445dd5a524cd2e2b31879b2
sugyeonghh <sugyeong.py@gmail.com>
2022-12-02 00:07:35 +0900

    치킨 쿠폰

--- ChickenCoupon.java
+++ ChickenCoupon.java
@@ -1,15 +1,16 @@
+// 프로그래머스 - 치킨 쿠폰
+
 public class ChickenCoupon {
 	public int solution(int chicken) {
-		int last = chicken % 10;
-		int count = chicken / 10;
-		chicken /= 10;
+		int coupon = chicken;
+		int count = 0;
 
-		while (chicken > 0) {
-			last += chicken % 10;
-			count += chicken / 10;
-			chicken /= 10;
+		while (coupon >= 10) {
+			int leftCoupon = coupon % 10;
+			count += coupon / 10;
+			coupon = leftCoupon + coupon / 10;
 		}
-		return count + last/10;
+		return count;
 	}
 
 	public static void main(String[] args) {
@@ -17,16 +18,5 @@ public class ChickenCoupon {
 		System.out.println(chickenCoupon.solution(100));    // 11
 		System.out.println(chickenCoupon.solution(1081));    // 120
 		System.out.println(chickenCoupon.solution(1999));    // 222
-
-		/*
-		* 1999 -> 1999장 -> 199마리, 9장
-		* 199 -> 199장 -> 19마리, 9장
-		* 19 -> 19장 -> 1마리, 9장
-		* 1마리 -> 1장 -> 1장
-		* 9장 + 9장 + 9장 + 1장 = 28장
-		* 28 -> 2마리
-		* 199+19+1+2 = 221마리
-		* */
-
 	}
 }
