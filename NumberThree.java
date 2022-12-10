4b51dc94fc3840951cf44725f373c85810ab6601
sugyeonghh <sugyeong.py@gmail.com>
2022-12-11 00:26:14 +0900

    저주의 숫자 3

--- NumberThree.java
+++ NumberThree.java
@@ -2,12 +2,21 @@
 
 public class NumberThree {
 	public int solution(int n) {
-		if (n % 3 == 0)
+		int result = 0;
+		for (int i = 1; i <= n; i++) {
+			result += 1;
+			while (!isValid(result)) result += 1;
+		}
+		return result;
+	}
 
+	private boolean isValid(int num) {
+		return num % 3 != 0 && !String.valueOf(num).contains("3");
 	}
 
 	public static void main(String[] args) {
 		NumberThree numberThree = new NumberThree();
+		System.out.println(numberThree.solution(10));   // 16
 		System.out.println(numberThree.solution(15));   // 25
 		System.out.println(numberThree.solution(40));   // 76
 	}
