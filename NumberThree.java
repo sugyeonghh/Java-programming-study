99cf2aa5527359efd0d4ee11b7807d3c9c3e5146
sugyeonghh <sugyeong.py@gmail.com>
2022-12-10 23:52:57 +0900

    저주의 숫자 3

---
+++ NumberThree.java
@@ -0,0 +1,14 @@
+// 프로그래머스 - 저주의 숫자 3
+
+public class NumberThree {
+	public int solution(int n) {
+		if (n % 3 == 0)
+
+	}
+
+	public static void main(String[] args) {
+		NumberThree numberThree = new NumberThree();
+		System.out.println(numberThree.solution(15));   // 25
+		System.out.println(numberThree.solution(40));   // 76
+	}
+}
