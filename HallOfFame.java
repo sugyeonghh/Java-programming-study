1c29a7036a254b51d5675234e8a81d316fdcef12
sugyeonghh <sugyeong.py@gmail.com>
2022-12-13 01:05:52 +0900

    프로그래머스 - 명예의 전당(1)

--- HallOfFame.java
+++ HallOfFame.java
@@ -3,7 +3,8 @@
 import java.util.*;
 
 public class HallOfFame {
-	public int[] solution(int k, int[] score) {
+	// ArrayList
+	public int[] solution2(int k, int[] score) {
 		int[] result = new int[score.length];
 		ArrayList<Integer> rank = new ArrayList<>();
 
@@ -11,6 +12,19 @@ public class HallOfFame {
 			rank.add(score[i]);
 			if (i >= k) rank.remove(Collections.min(rank));
 			result[i] = Collections.min(rank);
+		}
+		return result;
+	}
+
+	// priorityQueue
+	public int[] solution(int k, int[] score) {
+		int[] result = new int[score.length];
+		PriorityQueue<Integer> rank = new PriorityQueue<>();
+
+		for (int i = 0; i < score.length; i++) {
+			rank.add(score[i]);
+			if (i >= k) rank.poll();
+			result[i] = rank.peek();
 		}
 		return result;
 	}
