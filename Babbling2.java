24b4bd36fea36c96f14964f12df98476c802b982
sugyeonghh <sugyeong.py@gmail.com>
2022-12-30 18:38:58 +0900

    failed

---
+++ Babbling2.java
@@ -0,0 +1,28 @@
+// 프로그래머스 - 옹알이(2)
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Babbling2 {
+//	String[] babblingList = new String[]{"aya", "ye", "woo", "ma"};
+//	boolean[] visited = new boolean[4];
+//	ArrayList<String> combi = new ArrayList<>();
+//
+//	int n = babblingList.length;
+//
+//	public int solution(String[] babbling) {
+//
+//		return 0;
+//	}
+//
+//	private ArrayList<String> combination(ArrayList<String> combi, boolean visited, int r) {
+//
+//		return null;
+//	}
+
+	public static void main(String[] args) {
+//		Babbling2 babbling2 = new Babbling2();
+
+
+	}
+}
