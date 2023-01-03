c5fdfbace84c2803720d74cf6a08420cf8dbaa02
sugyeonghh <sugyeong.py@gmail.com>
2023-01-03 23:23:29 +0900

    백준 16637 괄호 추가하기

---
+++ AddParenthesis.java
@@ -0,0 +1,15 @@
+// 백준 16637 괄호 추가하기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AddParenthesis {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split("");
+
+
+	}
+}
