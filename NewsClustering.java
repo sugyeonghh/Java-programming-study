fa0ad451e8d1c78da0edbc37629c5852c8b46c51
sugyeonghh <sugyeong.py@gmail.com>
2023-01-30 18:22:47 +0900

    뉴스 클러스터링 수정

--- NewsClustering.java
+++ NewsClustering.java
@@ -1,39 +1,12 @@
 // 프로그래머스 - 뉴스 클러스터링
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class NewsClustering {
 	public int solution(String str1, String str2) {
-		str1 = str1.toLowerCase();
-		str2 = str2.toLowerCase();
-		Map<String, Integer> map1 = new HashMap<>();
-		Map<String, Integer> map2 = new HashMap<>();
-
-		Set<String> set = new HashSet<>();
-		char[] arr1 = str1.toCharArray();
-		char[] arr2 = str2.toCharArray();
-		for (int i = 0; i < arr1.length - 1; i++) {
-			if (Character.isAlphabetic(arr1[i]) && Character.isAlphabetic(arr1[i + 1])) {
-				String tmp = "" + arr1[i] + arr1[i + 1];
-				map1.put(tmp, map1.getOrDefault(tmp, 0) + 1);
-				set.add(tmp);
-			}
-		}
-		for (int i = 0; i < arr2.length - 1; i++) {
-			if (Character.isAlphabetic(arr2[i]) && Character.isAlphabetic(arr2[i + 1])) {
-				String tmp = "" + arr2[i] + arr2[i + 1];
-				map2.put(tmp, map2.getOrDefault(tmp, 0) + 1);
-				set.add(tmp);
-			}
-		}
-
-		int total = 0;
-		for (String s : set) {
-			total += Math.max(map1.getOrDefault(s, 0), map2.getOrDefault(s, 0));
-		}
+		Map<String, Integer> map1 = getMap(str1.toLowerCase());
+		Map<String, Integer> map2 = getMap(str2.toLowerCase());
 
 		int intersection = 0;
 		for (String s : map1.keySet()) {
@@ -41,8 +14,25 @@ public class NewsClustering {
 				intersection += Math.min(map1.get(s), map2.get(s));
 		}
 
+		int total = 0;
+		total += map1.keySet().stream().map(map1::get).mapToInt(n -> n).sum();
+		total += map2.keySet().stream().map(map2::get).mapToInt(n -> n).sum();
+		total -= intersection;
+
 		double result = total == 0 ? 1 : (double)intersection / (double)total;
 		return (int)(result * 65536);
+	}
+
+	private static Map<String, Integer> getMap(String str) {
+		Map<String, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (Character.isAlphabetic(str.charAt(i)) && Character.isAlphabetic(str.charAt(i + 1))) {
+				String tmp = "" + str.charAt(i) + str.charAt(i + 1);
+				map.put(tmp, map.getOrDefault(tmp, 0) + 1);
+			}
+		}
+		return map;
 	}
 
 	public static void main(String[] args) {
