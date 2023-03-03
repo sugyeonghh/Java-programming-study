c8f53454f7c7d83a9f43f7585dc9076eba15f1c3
sugyeonghh <sugyeong.py@gmail.com>
2023-03-03 23:59:03 +0900

    n진수 게임

---
+++ RadixGame.java
@@ -0,0 +1,12 @@
+public class RadixGame {
+    public String solution(int n, int t, int m, int p) {
+        String result = "";
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
