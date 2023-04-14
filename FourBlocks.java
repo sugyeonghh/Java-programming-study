d953bc8cc1301c11cafe63da0c151608d3afc15b
sugyeonghh <sugyeong.py@gmail.com>
2023-04-14 18:54:48 +0900

    프렌즈 4블록(카카오)

---
+++ FourBlocks.java
@@ -0,0 +1,14 @@
+// 프로그래머스 - 프렌즈 4블록(카카오)
+
+public class FourBlocks {
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FourBlocks fourBlocks = new FourBlocks();
+        System.out.println(fourBlocks.solution(4, 5, new String[]{"CCBDE", "AAADE", "AAABF", "CCBBF"}));
+    }
+}
